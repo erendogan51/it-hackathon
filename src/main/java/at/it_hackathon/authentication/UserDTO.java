a3135b27743d7f44676a067acd24121b49f5fc66
erendogan51 <93084571+erendogan51@users.noreply.github.com>
2022-05-22 12:42:30 +0200

    updates the userDTO to also return the role

--- src/main/java/at/it_hackathon/authentication/UserDTO.java
+++ src/main/java/at/it_hackathon/authentication/UserDTO.java
@@ -2,6 +2,7 @@ package at.it_hackathon.authentication;
 
 
 import at.it_hackathon.user.User;
+import at.it_hackathon.user.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,13 +15,16 @@ public class UserDTO {
     private final String email;
     private final int points;
 
+    private final UserRole userRole;
+
 
     public static UserDTO userToDTO(User user) {
         return new UserDTO(
                 user.getId(),
                 user.getUsername(),
                 user.getEmail(),
-                user.getPoints()
+                user.getPoints(),
+                user.getUserRole()
                 );
     }
 }
